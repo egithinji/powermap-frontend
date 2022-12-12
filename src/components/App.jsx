b176f450dd24035d8d7232b4a8760d2e7a6b440b
Eric Githinji <ericgithinji@gmail.com>
2022-12-12 13:06:22 +0300

    Make layout responsive for small screens.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,4 @@
 /* eslint-disable react/react-in-jsx-scope */
-import { useState } from 'react'
 import Map from "./Map";
 import SideBar from "./SideBar";
 import Stats from './Stats';
@@ -7,12 +6,12 @@ import Stats from './Stats';
 function App() {
 
   return (
-    <div className="container relative m-0 min-h-screen min-w-full bg-slate-300">
-        <div className="absolute top-0 left-0 z-20">
-            <SideBar />
-        </div>
-        <div className='min-h-screen min-w-full'>
+    <div className="container flex flex-col relative min-h-screen min-w-full bg-slate-300 m-0">
+        <div className='h-auto w-full sm:min-h-screen sm:min-w-full'>
             <Map />
+        </div>
+        <div className="sm:absolute sm:top-0 sm:left-0 z-20">
+            <SideBar />
         </div>
         <div className="absolute top-0 right-0 z-20 mr-4 mt-4">
             <Stats />
