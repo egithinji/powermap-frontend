e0a0ec334cd7e29a6ed4e8bc68735bbec15ebe4f
Eric Githinji <ericgithinji@gmail.com>
2022-12-27 18:33:31 +0300

    Comment out mocks setup.

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './components/App'
 import '../index.css'
 
-if (process.env.NODE_ENV === 'development') {
+/*if (process.env.NODE_ENV === 'development') {
   const {worker} = await import("./mocks/browser")
   worker.start()
-}
+}*/
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>
 )
