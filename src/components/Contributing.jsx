c64dfb7af9d0ce81c1590031bb33dd5002308450
Eric Githinji <ericgithinji@gmail.com>
2023-01-01 15:03:33 +0300

    Fix wording.

--- src/components/Contributing.jsx
+++ src/components/Contributing.jsx
@@ -15,13 +15,10 @@ function Contributing() {
             <div className=" text-lg flex justify-start gap-2 font-bold font-serif w-24">Contributing<FontAwesomeIcon className="cursor-pointer" icon={contVisible ? faCaretDown : faCaretRight} size="lg" onClick={handleClick} /></div>
             <div className={contVisible ? `transition-all duration-1000 ease-linear flex flex-col max-h-96 text-xs` : `transition-all duration-500 ease-linear max-h-0 overflow-hidden`}>
                 <div>
-                    <h3>Developers</h3>
-                    <div>
-                        <p>If you want to contribute to the frontend, visit our  <a href=' https://github.com/egithinji/flux-frontend'>frontend repo</a>.
-                        If you're more interested in the backend side of things checkout the  <a href=' https://www.google.com'>backend repo</a>.
-                        </p>
-                        <p>You can also hang out at our <a href='www.google.com'>discord</a>.</p>
-                    </div>
+                    <p>Contributions are very welcome! If you would like to contribute to the frontend, visit our  <a href='https://github.com/egithinji/powermap-frontend'>frontend repo</a>.
+                    If you're more interested in the backend side of things checkout the  <a href='https://github.com/egithinji/powermap-backend'>backend repo</a>.
+                    </p>
+                    <p>You can also hang out at our <a href='www.google.com'>discord</a>.</p>
                 </div>
             </div>
         </div>
