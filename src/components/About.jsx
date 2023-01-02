492981b4eefa768d8bdadcab2ede72a409bbb66a
Eric Githinji <ericgithinji@gmail.com>
2023-01-02 09:17:49 +0300

    Update About.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -15,8 +15,8 @@ function About() {
             <div className=" text-lg flex justify-start gap-2 font-bold font-serif items-center">About<FontAwesomeIcon className="cursor-pointer" icon={aboutVisible ? faCaretDown : faCaretRight} size="lg" onClick={handleClick} /></div>
             <div className={aboutVisible ? `transition-all duration-1000 ease-linear flex flex-col max-h-96 text-xs` : `transition-all duration-500 ease-linear max-h-0 overflow-hidden`}>
                 <div>
-                    <p>Power Map is a free service that provides members of the public with power outage data.</p>
-                    <p>It uses the Twitter API to track public tweets directed at the official <a href='https://www.kplc.co.ke/'>Kenya Power</a> twitter handle.</p>
+                    <p>Power Map uses the Twitter API to track public tweets directed at the official <a href='https://www.kplc.co.ke/'>Kenya Power</a> twitter handle.</p>
+                    <p>When a tweet contains a complaint about an area, a random point is generated on the map within that area.</p>
                 </div>
             </div>
         </div>
