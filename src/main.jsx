f828087f57f1eba1cfb80fb54740bbbd37a97aa6
Eric Githinji <ericgithinji@gmail.com>
2023-02-03 18:07:31 +0300

    Enable MSW if in development.

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './components/App'
+import App from './App'
 import '../index.css'
 
-/*if (process.env.NODE_ENV === 'development') {
+
+if (process.env.NODE_ENV === 'development') {
   const {worker} = await import("./mocks/browser")
   worker.start()
-}*/
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
